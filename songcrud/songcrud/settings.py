91e60cbcb6f6f9e8f40a94be644f88bf6411fc5d
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-10-29 11:31:18 +0000

    updated settings

--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%s(y!t*c#m!hvzghu#+=-s!9pt7j=o45(#+4cdf3(xfi6n^%vt'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'musicapp',
+    
 ]
 
 MIDDLEWARE = [
